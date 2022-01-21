6e2d615b73da7113c65c5180189893fd5c144fd3
dufellipeR <eduardo.fellipe30@gmail.com>
2022-01-21 13:57:40 -0300

    add customer filter

--- customer/models.py
+++ customer/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Customer(models.Model):
   name = models.CharField(max_length=255)
   code = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
--- customer/views.py
+++ customer/views.py
@@ -2,7 +2,10 @@ from customer.serializers import CustomerSerializer
 from django.shortcuts import render
 from .models import Customer
 from rest_framework import serializers, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
 class CustomerViewSet(viewsets.ModelViewSet):
   queryset = Customer.objects.all()
   serializer_class = CustomerSerializer
+  filter_backends = [DjangoFilterBackend]
+  filterset_fields = ['code']
